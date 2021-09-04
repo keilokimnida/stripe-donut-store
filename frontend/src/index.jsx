fc4f8497a62fe7f3b501d7a9caed77d7aebdd522
keilokimnida <tkl48leon@gmail.com>
2021-09-04 14:13:24 +0800

    Initialization of project

---
+++ frontend/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes.js';
+import './assets/scss/main.scss';
+
+ReactDOM.render(
+  <>
+    <Routes />
+  </>,
+  document.getElementById('eISO-root')
+);
